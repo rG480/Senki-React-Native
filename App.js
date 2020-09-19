2062250ebb7957cd9a4141ff29b5b1a913d0160b
Konstantinos Houtas <reyd0905@gmail.com>
2020-09-19 17:47:09 -0400

    Created basic sqlite integration - Created test control panel for messing with SQLite functionality. - Ability to add items - Ability to remove items - Ability to view database

--- App.js
+++ App.js
@@ -3,17 +3,21 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider as PaperProvider, TextInput, Button } from 'react-native-paper'
 import axios from 'axios'
+import * as SQLite from 'expo-sqlite'
 
 import keys from './keys.json'
 
 // Main app component. All of this is subject to change, as this is primarily to get the basic API connected.
+
+let db = SQLite.openDatabase("senki");
 
 export default class App extends React.Component {
   
   // State which contains input for the text box and data for the API
   state = {
     input_text: '',
-    data: []
+    data: [],
+    sqlite_db_content: ''
   }
 
   // Retrieves all API data based on a specific food input
@@ -22,11 +26,78 @@ export default class App extends React.Component {
     .then(api_data => {
       this.setState({
         data: api_data.data.hints
-      })
+      });
     })
     .catch(err => {
-      console.log(err)
+      console.log(err);
     })
+  }
+
+  addToDatabase() {
+    db.transaction(
+      tx => {
+        tx.executeSql(
+          "INSERT INTO items (value) VALUES (?);", [this.state.input_text]
+        );
+
+        tx.executeSql(
+          "SELECT * FROM items", [], (_, { rows }) => 
+          this.setState({
+            sqlite_db_content: JSON.stringify(rows)
+          })
+        )
+      }
+    )
+  }
+
+  resetTable(tableName) {
+    db.transaction(
+      tx => {
+        tx.executeSql(
+          `DELETE FROM ${tableName};`
+        );
+      }
+    )
+  }
+
+  componentDidMount() {
+    db.transaction(
+      tx => {
+        tx.executeSql(
+          "CREATE TABLE IF NOT EXISTS items(id integer primary key not null, value varchar(255));"
+        );
+
+        tx.executeSql(
+          "SELECT * FROM items", [], (_, { rows }) =>
+          this.setState({
+            sqlite_db_content: JSON.stringify(rows)
+          })
+        )
+      }
+    );
+  }
+
+  viewDatabase() {
+    db.transaction(
+      tx => {
+        tx.executeSql(
+          "SELECT * FROM items", [], (_, { rows }) =>
+          this.setState({
+            sqlite_db_content: JSON.stringify(rows)
+          })
+        )
+      }
+    )
+
+    let parsedJSON = JSON.parse(this.state.sqlite_db_content)._array
+    var text = ''
+
+    for (let i = 0; i < parsedJSON.length; i++) {
+        if (parsedJSON[i].value != undefined)
+          text += parsedJSON[i].value + '\n'
+    }
+
+    alert(text)
   }
 
   render() {
@@ -41,6 +112,9 @@ export default class App extends React.Component {
           />
 
           <Button style={styles.button} icon="check-underline" mode="contained" onPress={() => this.fetchApi()}>Go</Button>
+          <Button style={styles.button} icon="database-plus" mode="contained" onPress={() => this.addToDatabase()}>Add</Button>
+          <Button style={styles.button} icon="database-search" mode="contained" onPress={() => this.viewDatabase()}>View Database</Button>
+          <Button style={styles.button} icon="database-search" mode="contained" onPress={() => this.resetTable("items")}>Delete All Items</Button>
           
           {
             this.state.data.map(item => (
